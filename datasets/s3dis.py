4e830d1bc8e5d6f2ac1141dc51761b796eaf7048
Zach Ghera <zpghera00@gmail.com>
2021-10-15 19:43:01 -0400

    Refactor s3dis dataset module.
    No longer using absl flags now that we are using config class from the original implementation and add class that holds both the train and the test set.

--- datasets/s3dis.py
+++ datasets/s3dis.py
@@ -1,5 +1,5 @@
 """PVCNN S3DIS Configuration and Data Pipeline"""
-from typing import Dict, Any, Tuple, List
+from typing import Tuple, List
 from pathlib import Path
 from absl import flags
 import tensorflow as tf
@@ -9,35 +9,7 @@ FLAGS = flags.FLAGS
 AUTOTUNE = tf.data.AUTOTUNE
 
 
-def define_flags() -> None:
-  """Defining all the necessary flags."""
-  # pylint: disable=line-too-long
-  # fmt: off
-  flags.DEFINE_string("data_dir", "./data/s3dis/pointcnn/", "Directory to store the dataset")
-  flags.DEFINE_integer("shuffle_size", 50000, "Shuffle buffer size")
-  flags.DEFINE_integer("train_batch_size", 32, "Training batch size")
-  flags.DEFINE_integer("eval_batch_size", 10, "Evaluation batch size")
-  flags.DEFINE_integer("num_points", 8192, "Number of points to process for each scene")
-  flags.DEFINE_boolean("use_normalized_coords", True, "Whether include the normalized coords in feature")
-  flags.DEFINE_integer("holdout_area", 5, "Area to hold out for testing")
-  # fmt: on
-
-
-def get_flags_dict() -> Dict[str, Any]:
-  """Returns the command line arguments as a dict."""
-  kwargs = {
-    "data_dir": FLAGS.data_dir,
-    "shuffle_size": FLAGS.shuffle_size,
-    "train_batch_size": FLAGS.train_batch_size,
-    "eval_batch_size": FLAGS.eval_batch_size,
-    "num_points": FLAGS.num_points,
-    "use_normalized_coords": FLAGS.use_normalized_coords,
-    "holdout_area": FLAGS.holdout_area,
-  }
-  return kwargs
-
-
-def create_dataset(
+def create_s3dis_dataset(
   data_dir: str,
   shuffle_size: int,
   batch_size: int,
@@ -49,12 +21,7 @@ def create_dataset(
   """Creates train or test `tf.data.Dataset`.
   Args:
     is_train_split: True if create train dataset. False if create test dataset.
-    data_dir: Directory where prepared dataset is stored.
-    shuffle_size: Shuffle buffer size.
-    batch_size: Batch size.
-    num_points: Number of points to process for each scene.
-    use_normalized_coords: Whether include the normalized coords in features.
-    holdout_area: Area to hold out for testing.
+    See S3DIS.__init__ for other arguments.
   """
   filenames = _get_filenames(is_train_split, data_dir, holdout_area)
   filenames_ds = tf.data.Dataset.from_tensor_slices(filenames)
@@ -136,7 +103,6 @@ def _random_sample_data(
 
   if data_num_points < desired_num_points:
     # Sample with replacement
-    tf.print("sample with replacement", data_num_points, desired_num_points)
     indices = tf.random.uniform(
       shape=[desired_num_points],
       minval=0,
@@ -145,21 +111,60 @@ def _random_sample_data(
     )
   else:
     # Sample without replacement courtesy of https://github.com/tensorflow/tensorflow/issues/9260#issuecomment-437875125
-    tf.print("sample without replacement", data_num_points, desired_num_points)
     logits = tf.zeros([data_num_points])  # Uniform distribution
     # pylint: disable=invalid-unary-operand-type
     z = -tf.math.log(-tf.math.log(tf.random.uniform(tf.shape(logits), 0, 1)))
     _, indices = tf.nn.top_k(logits + z, k=desired_num_points)
-  tf.print("indices=", indices)
 
-  tf.print("PRE data shape=", tf.shape(data))
   data = tf.transpose(tf.gather(data, indices=indices))
-  tf.print("POST data shape=", tf.shape(data))
-  tf.print("PRE label shape=", tf.shape(label))
   label = tf.gather(label, indices=indices)
-  tf.print("POST label shape=", tf.shape(label))
   if use_normalized_coords:
     # data[9, num_points] =  [x_in_block, y_in_block, z_in_block, r, g, b, x / x_room, y / y_room, z / z_room]
     return data, label
   # data[6, num_points] = [x_in_block, y_in_block, z_in_block, r, g, b]
   return data[:-3, :], label
+
+
+class S3DIS(dict):
+  """
+  Holds the train and/or test split `tf.data.Dataset` object(s) for the S3DIS
+  dataset.
+  """
+
+  def __init__(
+    self,
+    data_dir: str,
+    shuffle_size: int,
+    batch_size: int,
+    num_points: int,
+    use_normalized_coords: bool,
+    holdout_area: int,
+    split=None,
+  ):
+    """
+    Initialize the `tf.data.Dataset` object(s) for the split(s) specified.
+    Args:
+      data_dir: Directory where prepared dataset is stored.
+      shuffle_size: Shuffle buffer size.
+      batch_size: Batch size.
+      num_points: Number of points to process for each scene.
+      use_normalized_coords: Whether include the normalized coords in features.
+      holdout_area: Area to hold out for testing.
+      split: 'train', 'test', or None. None will create both the train and
+             test splits.
+    """
+    super().__init__()
+    if split is None:
+      split = ["train", "test"]
+    elif not isinstance(split, (list, tuple)):
+      split = [split]
+    for s in split:
+      self[s] = create_s3dis_dataset(
+        data_dir,
+        shuffle_size,
+        batch_size,
+        num_points,
+        use_normalized_coords,
+        holdout_area,
+        is_train_split=(split == "train"),
+      )
