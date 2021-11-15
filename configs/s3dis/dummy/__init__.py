a3f9537568ccd1f6bce32998f48debbb9d4d714a
Zach Ghera <zpghera00@gmail.com>
2021-11-15 15:54:31 -0500

    Create dummy exp config for testing train.py.

---
+++ configs/s3dis/dummy/__init__.py
@@ -0,0 +1,23 @@
+"""Experiment configurations for a dummy model."""
+import tensorflow as tf
+
+from utils.config import Config, configs
+
+# dataset
+configs.dataset.num_points = 4096
+configs.dataset.holdout_area = 5
+
+# dummy model
+N = configs.dataset.num_points
+CL =  configs.data.num_classes
+# B = configs.dataset.batch_size
+# C = 9 if configs.dataset.use_normalized_coords else 6
+
+# inputs has shape [B, C, N].
+dummy_model = tf.keras.Sequential()
+dummy_model.add(tf.keras.layers.Reshape((-1)))
+dummy_model.add(tf.keras.layers.Dense(CL * N))
+# output has shape [B, num_classes=13, N].
+dummy_model.add(tf.keras.layers.Reshape((CL, N)))
+
+configs.model = Config(dummy_model)
