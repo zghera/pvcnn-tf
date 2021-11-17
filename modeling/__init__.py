d1aa2411b00737aa05c580d7b811e6bfaa6bde10
Zach Ghera <zpghera00@gmail.com>
2021-11-17 12:41:44 -0500

    Modifying imports for modeling module.

--- modeling/__init__.py
+++ modeling/__init__.py
@@ -1,1 +1,14 @@
-from modeling.s3dis import *
+"""Experiment configurations for PVCNN models."""
+from modeling.s3dis import PVCNN
+from modeling.layers import (
+  PointFeaturesBranch,
+  CloudFeaturesBranch,
+  ClassificationHead,
+)
+
+__all__ = [
+  "PVCNN",
+  "PointFeaturesBranch",
+  "CloudFeaturesBranch",
+  "ClassificationHead",
+]
