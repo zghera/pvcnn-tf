89293d119546f5f174136b85ac056dea46775425
Zach Ghera <zpghera00@gmail.com>
2021-11-17 14:25:54 -0500

    Making tf const didn't work, lets look at orig error again.

--- modeling/layers/voxelization.py
+++ modeling/layers/voxelization.py
@@ -50,9 +50,7 @@ class Voxelization(tf.keras.layers.Layer):
     self._norm_coords = tf.clip_by_value(self._norm_coords * R, 0, R - 1)
     self._vox_coords = tf.cast(tf.round(self._norm_coords), dtype=tf.int32)
 
-    vox_features_sqzd, _, _ = avg_voxelize(
-      features, self._vox_coords, tf.constant(R)
-    )
+    vox_features_sqzd, _, _ = avg_voxelize(features, self._vox_coords, R)
     voxelized_features = tf.reshape(vox_features_sqzd, shape=(B, C, R, R, R))
 
     return voxelized_features, self._norm_coords
