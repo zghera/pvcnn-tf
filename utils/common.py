6658eaf684518758db7272f1265c1c80041c18e7
Zach Ghera <zpghera00@gmail.com>
2021-11-18 10:02:51 -0500

    Set the single Colab GPU as the only visible GPU device.

--- utils/common.py
+++ utils/common.py
@@ -38,7 +38,17 @@ def config_gpu():
   # TODO: The following may be useful but caused colab to crash initially
   # os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"
   # os.environ["TF_CPP_VMODULE"]="gpu_process_state=10,gpu_cudamallocasync_allocator=10"
-  gpu_devices = tf.config.list_physical_devices("GPU")
-  print(f"Num GPUs Available: {len(gpu_devices)}\n")
-  for device in gpu_devices:
-    tf.config.experimental.set_memory_growth(device, True)
+  gpus = tf.config.list_physical_devices("GPU")
+  print(f"Num GPUs Available: {len(gpus)}")
+  if gpus:
+    # Restrict TensorFlow to only use the first GPU
+    try:
+      for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+      tf.config.set_visible_devices(gpus[0], "GPU")
+      logical_gpus = tf.config.list_logical_devices("GPU")
+      print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+    except RuntimeError as e:
+      # Visible devices must be set before GPUs have been initialized
+      print(e)
+  print()
